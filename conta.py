a4c86245e477dfc106ebdaf79b6f8cf5f2f6554a
unknown <thiago_b_alves@outlook.com>
2023-08-24 18:03:28 -0300

    aulas

--- conta.py
+++ conta.py
@@ -20,3 +20,17 @@ class Conta:
     def transferencia(self, valor, destino):
         self.saque(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
